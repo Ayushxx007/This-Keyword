c4bb5cbe971194336b892a3958f6c1c31829e174
Ayush Kumawat <138853573+Ayushxx007@users.noreply.github.com>
2025-04-18 06:23:10 +0530

    Create asyncawait.js

---
+++ asyncawait.js
@@ -0,0 +1,54 @@
+
+// console.log("start");
+
+// const pro = new Promise((resolve,reject)=>{
+//  setTimeout(()=>{resolve(8);},2000);
+// });
+
+// function prom(){
+//   pro.then((data)=>{console.log(data)});
+//   console.log("namaste js")
+
+// }
+
+// prom();
+
+
+
+
+
+
+// // async function always return Promise
+// // if you explicitly  return a value, it will be wrapped in a Promise
+
+// async function  profunc(){
+//   console.log("hello world");
+//   const val = await pro; 
+//   console.log("namaste js1")      // await can only be used inside async function
+//   console.log(val);
+// }
+
+
+// profunc();
+
+// console.log("end");
+
+console.log("start");
+const p=new Promise((resolve,reject)=>{ resolve("yes")});
+
+
+async function xyz(){
+  
+ const val= await p;
+ console.log(val);
+}
+
+xyz();
+
+
+
+
+console.log("end")
+
+
+
